7f24781c7c38debab4e0e534eb0aee6d76b3e948
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-10-13 13:36:38 +0200

    dropdown

--- website-skeleton/webpack.config.js
+++ website-skeleton/webpack.config.js
@@ -28,7 +28,7 @@ Encore
     .addStyleEntry('scss/dropdown', './assets/styles/dropdown.scss')
     .addStyleEntry('scss/dropdownTheme1', './assets/styles/dropdownTheme1.scss')
 
-    .addStyleEntry('scss/dropdown', './assets/js/dropdown.js')
+    .addEntry('js/dropdownJS', './assets/js/dropdownJS.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
