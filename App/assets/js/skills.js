9de24acc03d76aab85cd2635fe412de10eadd372
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-06-15 16:13:09 +0200

    skill responsive modals

--- App/assets/js/skills.js
+++ App/assets/js/skills.js
@@ -1,7 +1,10 @@
 let skills = document.getElementsByClassName('skill')
-
+let closeModal = document.getElementsByClassName('closeModal')
 for (let i = 0; i <skills.length; i++){
     skills[i].addEventListener('click', ()=> {
-        document.getElementById(i).classList.add('activeModal')
+        skills[i].nextElementSibling.classList.add('activeModal')
+    })
+    closeModal[i].addEventListener('click', ()=> {
+        skills[i].nextElementSibling.classList.remove('activeModal')
     })
 }
