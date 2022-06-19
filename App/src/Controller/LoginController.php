d1824e78a3567f0dbba9599b9726304f1afaf91f
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-06-20 01:30:21 +0200

    login backend

---
+++ App/src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController {
+
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * @Route ("/login", name="login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+
+    public function login(AuthenticationUtils $authenticationUtils) : Response{
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('admin/login.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
