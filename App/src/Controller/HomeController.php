82aabec01e10d9bbe2e2dbb79afce06029cfaa44
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-06-19 23:12:14 +0200

    translate backend

--- App/src/Controller/HomeController.php
+++ App/src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
 
@@ -13,6 +15,21 @@ class HomeController extends AbstractController {
         return $this->render('pages/home.html.twig', [
 
         ]);
+    }
+
+
+    /**
+     * changement de la langue
+     * @route("/change-locale/{locale}", name="ChangeLocale")
+     * @param $locale
+     * @param Request $request
+     * @return Response
+     */
+    public function ChangeLocale($locale, Request $request) : Response{
+
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirect($request->headers->get('referer'));
     }
 
 
