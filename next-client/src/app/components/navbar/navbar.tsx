86dce9aaa548f3059592836729e475dd3e8814fb
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-08-19 01:29:16 +0200

    debut request

--- next-client/src/app/components/navbar/navbar.tsx
+++ next-client/src/app/components/navbar/navbar.tsx
@@ -1,8 +1,44 @@
+"use client";
+import {createDirectus, readItems} from '@directus/sdk';
+import {rest} from '@directus/sdk/rest';
+import {useEffect} from "react";
+
+const client = createDirectus('https://directus.oscarpalissot.fr').with(rest());
+
 
 interface NavbarProps {
 
 }
+
 export default function Navbar() {
+
+    useEffect(() => {
+        client.request(
+            readItems('homepage_content', {
+                deep: {
+                    translations: {
+                        _filter: {
+                            languages_code: {
+                                _eq: 'en-US'
+                            }
+                        },
+                    },
+                },
+                fields: ['Sections',
+                    {
+                        Sections: ['*',
+                            {
+                                item: ['*',
+                                    { translations: ['*']}
+                                ]
+                            }]
+                    }],
+            })
+        ).then(data => {
+            console.log(data)
+        })
+    }, [])
+
     return (
         <>
             <nav>
