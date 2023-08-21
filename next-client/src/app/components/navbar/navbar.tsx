aeffac8478a415f7291d6455808a8a1dc3ab56cb
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-08-21 20:22:31 +0200

    navbar

--- next-client/src/app/components/navbar/navbar.tsx
+++ next-client/src/app/components/navbar/navbar.tsx
@@ -1,16 +1,20 @@
 "use client";
 import {createDirectus, readItems} from '@directus/sdk';
 import {rest} from '@directus/sdk/rest';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
-const client = createDirectus('https://directus.oscarpalissot.fr').with(rest());
+import styles from './navbar.module.css'
 
+const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest());
 
-interface NavbarProps {
-
-}
 
 export default function Navbar() {
+
+    const [links, setLinks] = useState<string[]>([])
+    const [homePageContent, setHomePageContent] = useState<HomepageContent>()
+    const [logo, setLogo] = useState<string>('')
+    const [headerBorderClass, setHeaderBorderClass] = useState<string>('');
+    const [hamburger__toggle__class, setHamburger__toggle__class] = useState<string>(styles.hamburger__toggle);
 
     useEffect(() => {
         client.request(
@@ -18,39 +22,81 @@ export default function Navbar() {
                 deep: {
                     translations: {
                         _filter: {
-                            languages_code: {
-                                _eq: 'en-US'
-                            }
+                            languages_code: {_eq: 'fr-FR'},
                         },
                     },
                 },
-                fields: ['Sections',
-                    {
-                        Sections: ['*',
-                            {
-                                item: ['*',
-                                    { translations: ['*']}
-                                ]
-                            }]
-                    }],
+                fields: ['*', {
+                    translations: ['*', {
+                        Sections: ['*', {
+                            item: ['*']
+                        }]
+                    }]
+                }],
             })
-        ).then(data => {
-            console.log(data)
+        ).then(data  => {
+            setHomePageContent(data as unknown as HomepageContent)
         })
     }, [])
 
+    useEffect(() => {
+        if (homePageContent) {
+            setLinks(homePageContent.translations[0].Sections.map(section => section.item.Link))
+            setLogo(homePageContent.translations[0].Logo)
+        }
+    }, [homePageContent])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setHeaderBorderClass(styles.header__border__active);
+            } else {
+                setHeaderBorderClass('');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setHamburger__toggle__class(hamburger__toggle__class === styles.hamburger__toggle ? `${styles.hamburger__toggle__expanded} ${styles.hamburger__toggle}` : styles.hamburger__toggle)
+    }
+
     return (
         <>
-            <nav>
-                <div className="logo">
-                    <h1>Oscar PALISSOT</h1>
-                </div>
-                <div className="hamburger__toggle">
-                    <div className="hamburger__btn"></div>
-                </div>
-                <div>
+            <nav className={styles.header}>
 
+                <div className={styles.header__items}>
+
+                    <a href="/" >
+                        <img className={styles.header__logo} src={process.env.NEXT_PUBLIC_ASSETS_URL + logo + '?key=logo'} alt="logo"/>
+                    </a>
+
+                    <div className={hamburger__toggle__class} onClick={toggleMenu}>
+                        <div className={styles.hamburger__btn}></div>
+                    </div>
+
+                    <div className={styles.nav_menu}>
+                        <ul className="nav__list">
+                            {links.map((link, index) => {
+                                return (
+                                    <li key={index} className="nav__item">
+                                        <a href={link} className="nav__link">
+                                            {link}
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className={`${styles.header__border} ${headerBorderClass}`}></div>
                 </div>
+
+
             </nav>
         </>
     )
--- next-client/src/app/page.tsx
+++ next-client/src/app/page.tsx
@@ -1,10 +1,52 @@
-import Navbar from "@/app/components/navbar/navbar";
+'use client';
 
+import Navbar from "@/app/components/navbar/navbar";
+import {useEffect, useState} from "react";
+import {createDirectus, readItems} from "@directus/sdk";
+import {rest} from '@directus/sdk/rest';
+
+const client = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL!).with(rest());
 export default function Home() {
+
+    const [homePageContent, setHomePageContent] = useState<HomepageContent>()
+
+    useEffect(() => {
+        client.request(
+            readItems('homepage_content', {
+                deep: {
+                    translations: {
+                        _filter: {
+                            languages_code: {_eq: 'fr-FR'},
+                        },
+                    },
+                },
+                fields: ['*', {
+                    translations: ['*', {
+                        Sections: ['*', {
+                            item: ['*']
+                        }]
+                    }]
+                }],
+            })
+        ).then(data  => {
+            setHomePageContent(data as unknown as HomepageContent)
+        })
+    }, [])
     return (
         <>
             <Navbar/>
-            <h1>test</h1>
+
+            <p>Home page</p>
+            {homePageContent && homePageContent.translations[0].Sections.map(section => {
+                return (
+                    <div key={section.id}>
+                        <p>{section.item.Headline}</p>
+                        <p>{section.item.Content}</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                )
+            })}
+
         </>
     )
 }
