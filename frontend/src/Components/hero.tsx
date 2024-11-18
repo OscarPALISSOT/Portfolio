f0a993bc9d25c8a0d069e9574b72838a76f58816
OscarPALISSOT <oscar.p25000@gmail.com>
2024-11-18 18:33:14 +0100

    fix deployment

--- frontend/src/Components/hero.tsx
+++ frontend/src/Components/hero.tsx
@@ -8,17 +8,17 @@ interface HeroBlockProps {
 
 const HeroBlock =({heroBlock}: HeroBlockProps) => {
     return (
-        <div className={"flex flex-col lg:flex-row lg:items-center"}>
-            <div className={"flex flex-col lg:w-1/2"}>
+        <div className={"flex flex-col lg:flex-row lg:items-center lg:gap-6"}>
+            <div className={"flex flex-col lg:w-2/3"}>
                 <h1 className={"text-4xl lg:text-5xl font-semibold mb-3"}>{heroBlock.headline}</h1>
-                <h2 className={"text-2xl lg:text-3xl font-semibold mb-4 lg:mb-16"}>{heroBlock.sub_headline}</h2>
-                <p className={"text-lg lg:text-xl mb-4"}>{heroBlock.description}</p>
+                <h2 className={"text-2xl lg:text-3xl font-semibold mb-4 lg:mb-12"}>{heroBlock.sub_headline}</h2>
+                <p className={"text-lg lg:text-xl mb-4 lg:mb-12"}>{heroBlock.description}</p>
                 <Button
                     text={heroBlock.cv_button_label}
                 />
             </div>
-            <div className={"flex justify-center items-center lg:w-1/2"}>
-                <Image className={"hidden lg:block"} src={process.env.NEXT_PUBLIC_ASSETS_URL + heroBlock.image} alt="Oscar PALISSOT" width={500} height={420}/>
+            <div className={"flex justify-center items-center lg:w-1/3"}>
+                <Image className={"hidden lg:block w-full"} src={process.env.NEXT_PUBLIC_ASSETS_URL + heroBlock.image} alt="Oscar PALISSOT" width={500} height={420}/>
             </div>
         </div>
     )
