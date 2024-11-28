abef157548a769899ff7639ecafe57f0ddb99ebb
OscarPALISSOT <oscar.p25000@gmail.com>
2024-11-28 20:05:16 +0100

    fix responsive card

--- frontend/src/components/experienceBlock/card.tsx
+++ frontend/src/components/experienceBlock/card.tsx
@@ -8,21 +8,18 @@ type CardProps = {
 
 const Card = ({experience}: CardProps) => {
     return (
-        <Link href={"/experience/" + experience.id}>
-            <div
-                className={'mx-3 w-[300px] lg:w-[400px] 2xl:w-[500px] border border-border rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden cursor-pointer hover:scale-[102%]'}>
-                <div className={'h-[168px] lg:h-[224px] 2xl:h-[281px]  w-full'}>
-                    <Image
-                        className={'h-full w-full object-cover'}
-                        src={process.env.NEXT_PUBLIC_ASSETS_URL + experience.thumbnail}
-                        width={888}
-                        height={500}
-                        alt={experience.title}
-                    />
-                </div>
-                <div className={'p-4 w-full bg-light'}>
-                    <h3 className={'text-fontColor font-bold text-xl truncate'}>{experience.title}</h3>
-                </div>
+        <Link href={"/experience/" + experience.id} className={"mx-3 w-[300px] lg:w-[400px] 2xl:w-[500px] rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden cursor-pointer hover:scale-[102%]"}>
+            <div className={'h-[168px] lg:h-[224px] 2xl:h-[281px] w-full'}>
+                <Image
+                    className={'h-full w-full object-cover'}
+                    src={process.env.NEXT_PUBLIC_ASSETS_URL + experience.thumbnail}
+                    width={888}
+                    height={500}
+                    alt={experience.title}
+                />
+            </div>
+            <div className={'p-4 w-full bg-light'}>
+                <h3 className={'text-fontColor font-bold text-xl truncate'}>{experience.title}</h3>
             </div>
         </Link>
     )
